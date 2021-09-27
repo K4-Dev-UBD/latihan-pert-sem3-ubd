67762805fb20d28177d5cab2598b75cf362af479
Verdo Fanv <86637675+VerdoFanv@users.noreply.github.com>
2021-09-27 11:39:14 +0700

    v1.1

--- pert3/Root.cs
+++ pert3/Root.cs
@@ -7,13 +7,30 @@ namespace pert3
             Console.WriteLine("Program Inheritance, Access Modifier, Override\n");
             Mobil mobil = new Mobil();
             Motor motor = new Motor();
-            Truk truk = new Truk();
-            Pesawat pesawat = new Pesawat();
 
-            mobil.mulai();
-            motor.mulai();
-            truk.mulai();
-            pesawat.mulai();
+            Console.WriteLine("Mobil Merk " + mobil.merk);
+            Console.WriteLine("Mobil diproduksi tahun " + mobil.thnProduksi);
+            Console.WriteLine("Warna mobil " + mobil.warnaKendaraan);
+            Console.WriteLine("Tipe mobil " + mobil.tipe);
+            Console.WriteLine("Mobil memiliki roda " + mobil.jumlah_roda);
+            Console.WriteLine("Mobil memiliki kursi sebanyak " + mobil.kursi);
+            Console.WriteLine("Mobil memiliki pintu sebanyak " + mobil.pintu);
+
+            mobil.maju();
+            mobil.mundur();
+            mobil.belok();
+            mobil.berhenti();
+
+            Console.WriteLine("\n==================================\n");
+            Console.WriteLine("Motor merk " + motor.merk);
+            Console.WriteLine("Motor diproduksi tahun " + motor.thnProduksi);
+            Console.WriteLine("Warna motor " + motor.warnaKendaraan);
+            Console.WriteLine("Tipe motor " + motor.tipe);
+            Console.WriteLine("Motor memiliki roda " + motor.jumlah_roda);
+
+            motor.maju();
+            motor.berhenti();
+            motor.belok();
 
             Console.ReadKey(true);
         }
